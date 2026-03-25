19ac45e2331905e8a7ab09b6acd15e292e484caa
penpitchanuntago <68010826@kmitl.ac.th>
2026-03-25 10:23:28 +0700

    p00

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+//import  purchase from "../data"
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return "Hello NestJS";
   }
 }
--- src/products/Product.Controller.ts
+++ src/products/Product.Controller.ts
@@ -1,2 +1,2 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+//import { Controller, Get } from '@nestjs/common';
+//import { ProductService } from './';
